48f380070da4196a656b0c4b7b84a453105d0a00
Alejandro R. Mosteo <amosteo@unizar.es>
2019-10-20 00:42:51 +0200

    Iterators for Lists

---
+++ src/iterators-from-lists.ads
@@ -0,0 +1,43 @@
+with Ada.Containers.Doubly_Linked_Lists;
+
+with Iterators.Collectors.Sequences;
+with Iterators.Root;
+with Iterators.Traits.Containers.Appendable;
+
+generic
+   with package Ada_Containers is new Ada.Containers.Doubly_Linked_Lists (<>);
+package Iterators.From.Lists with Preelaborate is
+
+   subtype Container is Ada_Containers.List;
+   subtype Elements  is Ada_Containers.Element_Type;
+
+   package Container_Traits is new Traits.Containers
+     (Container                   => Container,
+      Element_Type                => Ada_Containers.Element_Type,
+      Cursor                      => Ada_Containers.Cursor,
+      First                       => Ada_Containers.First,
+      Next                        => Ada_Containers.Next,
+      Has_Element                 => Ada_Containers.Has_Element,
+      Reference_Type              => Ada_Containers.Reference_Type,
+      Reference                   => Ada_Containers.Reference,
+      Constant_Reference_Type     => Ada_Containers.Constant_Reference_Type,
+      Constant_Reference          => Ada_Containers.Constant_Reference);
+
+   package Appendable_Traits is new Container_Traits.Appendable
+     (Ada_Containers.Append);
+
+   package Iterators is new Standard.Iterators.Root (Elements);
+   --  This package provides the regular sources, operators, and sinks.
+
+   package Collectors is new Standard.Iterators.Collectors.Sequences
+     (Iterators,
+      Container_Traits,
+      Appendable_Traits);
+   --  Provides collection back into the same container type.
+
+   package Generators is new Standard.Iterators.Generators
+     (Iterators,
+      Container_Traits);
+   --  Provides conversion from container into iterator.
+
+end Iterators.From.Lists;
